8fab25ad2ee8970d2a69fa4e7309562b5bc70df1
danielpcampagna <danielprett@gmail.com>
2018-12-28 18:12:35 -0200

    [Second commit] Add a external dependency

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 
 This test was built following [this tutorial](https://david-barreto.com/how-to-use-typescript-with-systemjs/).
 
+The second commit consideres [this tutorial](https://riptutorial.com/systemjs).
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import {Person} from './person';
+import moment from 'moment';
 
 let person = new Person();
 console.log(person.name);
+
+const m1 = moment().format('LLL');
+const m2 = moment().fromNow();
+console.log(`The moment is ${m1}, which was ${m2}`);
